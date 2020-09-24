4d40a5222ebbb16c108931247418325abeef7e26
James G <james@trainingdata.io>
2020-09-24 20:05:09 +0000

    install.sh - include nvidia-docker2

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 # get docker-compose yml
 #wget https://trainingdataio.s3.amazonaws.com/docker-compose.ngx.yml
+# pull trainingdata.io docker image
+# docker pull trainingdataio/tdviewer:v2.0-ngx
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey |   sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list |   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get install nvidia-docker2
 
 # pull nvidia docker image
 docker pull nvcr.io/nvidia/clara-train-sdk:v3.0   
@@ -9,5 +16,3 @@ docker pull nvcr.io/nvidia/clara-train-sdk:v3.0
 sudo apt-get install -y python3-pip
 
 pip3 install -r flaskserver/requirements.txt
-# pull trainingdata.io docker image
-# docker pull trainingdataio/tdviewer:v2.0-ngx
